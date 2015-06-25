d206016887bb45350abcaa92dd39ecfc688989f7
Andres Sandoval <android.sjsu@gmail.com>
2015-06-25 00:58:46 -0700

    First commit

---
+++ app/src/main/java/andres_sjsu/simpletodo/MainActivity.java
@@ -0,0 +1,120 @@
+package andres_sjsu.simpletodo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+    private final int REQUEST_CODE = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems =(ListView)findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second item");
+        setupListViewListener();
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+
+                String Item = (String) lvItems.getItemAtPosition(position);
+
+               Toast.makeText(getApplicationContext(), Item+  "  --view: " , Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,EditItemActivity.class);
+                //based on item add info to intent
+                intent.putExtra("Item", Item);
+                intent.putExtra("id", String.valueOf(id));
+                startActivityForResult(intent, REQUEST_CODE );
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            // Extract name value from result extras
+            String name = data.getExtras().getString("Item");
+            String id = data.getExtras().getString("id");
+            int code = data.getExtras().getInt("code", 0);
+            int i = Integer.parseInt(id);
+
+            items.remove(i);
+
+            itemsAdapter.insert(name, i);
+            itemsAdapter.notifyDataSetChanged();
+            writeItems();
+            // Toast the name to display temporarily on screen
+          //  Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onAddItem(View v)
+    {
+        EditText etNewItem = (EditText)findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void setupListViewListener(){
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+
+                        return true;
+                    }
+                });
+    }
+
+    //Method to read/write file
+    private void readItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try{
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e){
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try{
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
